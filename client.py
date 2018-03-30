7b0e3d5fed1dd2e90348bf8848f933a6a10bd306
Alex <alex@MacBook-Pro-admnt.local>
2018-03-30 10:20:37 +0300

    homework#1

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 import settings
 import json
+import sys
 
 
 class JSONResponse:
@@ -26,6 +27,14 @@ class JSONResponse:
         for key, value in headers.items():
             yield key, value
 
+    @property
+    def body(self):
+
+        # Read only свойство body
+        # Свойство body содержит тело ответа сервера
+        body = self._envelope.get('body')
+        return body
+
 
 class JSONRequest:
     def __init__(self, url, method, body, **headers):
@@ -49,7 +58,7 @@ class JSONRequest:
         envelope.update({'startline': start_line})
         envelope.update({'headers': self._headers})
         envelope.update({'body': self._body})
-        data_str = json.dump(envelope)
+        data_str = json.dumps(envelope)
         return data_str.encode(settings.ENCODING)
 
 
@@ -61,29 +70,29 @@ class EchoClient:
         self._sock = socket.socket()
 
         # Связываем сокет соединение с хостом и портом сервера 
-        self._sock.connect((settings.HOST, settings.PORT))
+        self._sock.connect((sys.argv[1], int(sys.argv[2])))
 
     def read(self, _sock):
 
         # Получаем данные с сервера
         bytes_data = self._sock.recv(settings.BUFFER_SIZE)
 
-        # Приводим полученные данные к строковому виду
-        str_data = bytes_data.decode(settings.ENCODING)
+        # декодируем байты в json
+        j_response = JSONResponse(bytes_data)
 
         # Выводим полученные данные на экран
-        print(str_data)
+        print(j_response.body)
 
     def write(self):
 
         # Вводим данные с клавиатуры
         str_data = input('Enter data: ')
 
-        # Приводим отправляемые данные к байтовому виду
-        bytes_data = str_data.encode(settings.ENCODING)
+        json_data = JSONRequest('url', 'method', str_data)
 
+        # Приводим отправляемые данные к байтовому виду
         # Отправляем данные на сервер
-        self._sock.send(bytes_data)
+        self._sock.send(json_data.to_bytes())
 
     def run(self):
 
--- server.py
+++ server.py
@@ -3,62 +3,101 @@ import select
 import settings
 import collections
 import json
+import optparse
+
+# Request-объект - используется для преобразования "сырых" данных (байтов) в python-объект
 
 
 class JSONRequest:
 
+    # Конструктор в качестве аргументов принимает исключительно "сырые" данные
+    # Внутри конструктора "сырые" данные преобразуются в словарь
     def __init__(self, message_bytes):
+
         message_str = message_bytes.decode(settings.ENCODING)
+
         self._envelope = json.loads(message_str)
-        self._startline = self._envelope.get('startline')
 
     @property
-    def uri(self):
-        uri = self._startline.get('uri')
-        return uri
+    def action(self):
 
-    @property
-    def method(self):
-        method = self._startline.get('method')
-        return method
+        # Read only свойство action
+        action = self._envelope.get('action')
+
+        return action
 
     @property
     def headers(self):
+
+        # Read only свойство headers
+        # Свойство headers содержит дополнительные данные о запросе, например время его совершения
         headers = self._envelope.get('headers')
+
         for key, value in headers.items():
             yield key, value
 
     @property
     def body(self):
+
+        # Read only свойство body
+        # Свойство body содержит тело запроса
         body = self._envelope.get('body')
+
         return body
 
 
+# Response-объект - используется для приведения python-объекта в байтовый вид (для генерации "сырых" данных)
 class JSONResponse:
 
-    def __init__(self, code, method, body, **headers):
+    # Конструктор в качестве аргументов принимает основные данные об ответе сервера
+    def __init__(self, code, action, body, **headers):
+
         self._headers = headers
-        self._method = method
+
+        self._action = action
+
         self._code = code
+
         self._body = body
 
+    # Метод add_header - используется для добавления дополнительных
+    # данных об товете сервера, например времени его совершения
     def add_header(self, key, value):
+
         self._headers.update({key: value})
 
+    # Метод add_header - используется для удаления дополнительных данных
+    #  об товете сервера, если во время его заполнения была допущена ошибка
     def remove_header(self, key):
+
         del self._headers[key]
 
+    # Метод to_bytes - используется для преобразования данных об ответе сервера в байты
     def to_bytes(self):
+
         envelope = dict()
-        start_line = dict()
-        start_line.update({'code': self._code})
-        start_line.update({'method': self._method})
-        start_line.update({'version': settings.VERSION})
-        envelope.update({'startline': start_line})
+
+        envelope.update({'code': self._code})
+
+        envelope.update({'action': self._action})
+
         envelope.update({'headers': self._headers})
+
         envelope.update({'body': self._body})
-        data_str = json.dump(envelope)
+
+        data_str = json.dumps(envelope)
+
         return data_str.encode(settings.ENCODING)
+
+
+# получаем ip и порт из командной строки, аргументами -i, -p
+parser = optparse.OptionParser()
+
+parser.add_option('-a', '--ip', action="store", dest="ip", help="query string", default="127.0.0.1")
+
+parser.add_option('-p', '--port', action="store", dest="port", help="query string", default="7777")
+
+options, args = parser.parse_args()
 
 
 class EchoServer:
@@ -75,7 +114,7 @@ class EchoServer:
         self._sock = socket.socket()
 
         # Связываем сокет соединение с хостом и портом сервера 
-        self._sock.bind((settings.HOST, settings.PORT))
+        self._sock.bind((options.ip, int(options.port)))
 
         # Ждем обращений клиентов 
         self._sock.listen(settings.CLIENTS_NUM)
@@ -103,24 +142,24 @@ class EchoServer:
             data = client.recv(settings.BUFFER_SIZE)
             # Если полученные данные не являются пустой строкой
             if data:
+                j_receive = JSONRequest(data)
                 # Приводим полученные данные к строковому виду
-                str_data = data.decode(settings.ENCODING)
+                str_data = j_receive.body
+
                 # Сохраняем запрос на сервере
                 self._requests.append(str_data)
 
         except ConnectionResetError:
-
             # В случае разрыва соединения с клиентом и наличии данного клиента в списке подключений
             if client in self._connections:
                 # Удаляем соответствующего клиента из списка подключений
                 self._connections.remove(client)
 
-    def write(self, client, request):
+    def write(self, client, str_data):
         try:
-            # Приводим отправляемые данные к байтовому виду
-            bytes_message = request.encode(settings.ENCODING)
+            j_response = JSONResponse('code', 'action', str_data)
             # Отправляем данные на клиент
-            client.send(bytes_message)
+            client.send(j_response.to_bytes())
         except (ConnectionResetError, BrokenPipeError):
             # В случае разрыва соединения с клиентом и наличии данного клиента в списке подключений
             if client in self._connections:
